43e27d8d7c38308cbf3c75fcf2e3968578886fed
unknown <mksunnybd@gmail.com>
2022-03-08 23:35:47 +0600

    abd

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const http = require("http")
 const app = express()
+const cors = require('cors');
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
 	cors: {
@@ -8,6 +9,8 @@ const io = require("socket.io")(server, {
 		methods: [ "GET", "POST" ]
 	}
 })
+
+app.use(cors());
 
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id)
